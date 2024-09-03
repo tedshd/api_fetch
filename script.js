3cb04391cc1f23d045c49f0c09fc61ee001e2ca2
ted.hsu <ted.hsu@authme.com>
2024-09-03 14:06:38 +0800

    [dev] Update fetch function.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const ERRORCODE = {
   'OFFLINE': 900,
   'NETWORKERROR': 901
 }
-const d4Header = {
+const d4Options = {
   headers: {
     'Content-Type': 'application/json',
     // 'Authorization': `Bearer ${accessToken}`,
@@ -32,6 +32,92 @@ const initUrlencoded = (obj) => {
     urlencoded.append(key, obj[key]);
   }
   return urlencoded;
+}
+
+const option = {
+  method:
+  url:
+  body:
+  queryString:
+  header: {
+
+  }
+}
+
+const apiFetch = async (arg) => {
+  const option = {
+    method: arg.method,
+    url: arg.url || '',
+    body: arg.body || '',
+    queryString: arg.queryString || '',
+    header: arg.method || {}
+  }
+  if (!option.method) {
+    console.error('apiFetch:', 'no set method');
+    return;
+  }
+  d4Options.headers = {
+    ...d4Options.headers,
+    ...option.header
+  };
+  if (option.method !== 'GET') {
+    d4Options.body = option.body
+  }
+  const apiURL = (option.method === 'GET') ? `${apiHost}/${option.url}?${option.queryString.toString()}` : `${apiHost}/${option.url}`
+  try {
+    const response = await fetch(apiURL, {
+      ...d4Options,
+      method: option.method
+    });
+    console.log('response', response)
+    if (!response.ok) {
+      switch (response.status) {
+        case 400:
+          throw new FetchError(response, {message: 'Bad Request' });
+        case 401:
+          throw new FetchError(response, { message: 'Unauthorized' });
+        case 403:
+          throw new FetchError(response, { message: 'Forbidden' });
+        case 404:
+          throw new FetchError(response, { message: 'Not Found' });
+        case 500:
+          throw new FetchError(response, { message: 'Internal Server Error' });
+        default:
+          throw new FetchError(response, { message: 'Unknown Error' });
+      }
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    if (error instanceof FetchError) {
+      throw error;
+    } else {
+      if (error instanceof TypeError && error.message === 'Failed to fetch') {
+        // Using navigator.onLine as an additional check
+        if (!navigator.onLine) {
+          console.error('Network error: You appear to be offline.');
+          throw new FetchError('Network error', {
+            message: 'offline',
+            code: ERRORCODE.OFFLINE
+          })
+        } else {
+          console.error('Network error: Connection was reset.');
+          throw new FetchError('Network error', {
+            message: 'network error',
+            code: ERRORCODE.NETWORKERROR
+          })
+        }
+      } else {
+        console.error('Unexpected error:', error);
+      }
+      console.log(error instanceof TypeError); // true
+      console.log(error.message); // "null has no properties"
+      console.log(error.name); // "TypeError"
+      console.log(error.stack); // Stack of the error
+      console.error('Unexpected error:', error);
+    }
+  }
+
 }
 
 const apiTest = async () => {
