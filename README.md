e096cb4cbe0e0d0604d18eec0757fec55cb1845e
ted.hsu <ted.hsu@authme.com>
2024-09-03 15:11:59 +0800

    [dev] Update README & code.

--- README.md
+++ README.md
@@ -7,17 +7,73 @@ handle fetch
 ```javascript
 
 try {
-
+  const res = await apiFetch({
+    method: '', // GET, POST, PUT, DELETE...
+    url: '',
+    body: {},
+    queryString: {},
+    header: {}
+  })
 } catch (e) {
-
+  console.log(e)
 }
+```
 
-const res = apiFetch({
-  method
-  url
-  body
-  queryString
-  header
-}) 
+## Use case
 
+### raw
+
+```javascript
+{
+  method: '',
+  url: '',
+  body: JSON.stringify({}),
+  queryString: {},
+  header: {}
+}
+```
+
+### application/x-www-form-urlencoded
+
+```javascript
+{
+  method: '',
+  url: '',
+  body: new URLSearchParams({}),
+  queryString: {},
+  header: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+}
+```
+
+### FormData
+
+```javascript
+const formData = new FormData();
+formData.append('key1', 'value1');
+
+{
+  method: '',
+  url: '',
+  body: formData,
+  queryString: {},
+  header: {}
+}
+```
+
+### upload file
+
+```javascript
+const fileInput = document.getElementById('fileInput');
+const formData = new FormData();
+formData.append('file', fileInput.files[0]);
+
+{
+  method: '',
+  url: '',
+  body: formData,
+  queryString: {},
+  header: {}
+}
 ```
--- script.js
+++ script.js
@@ -38,7 +38,7 @@ const apiFetch = async (arg) => {
   const option = {
     method: arg.method,
     url: arg.url || '',
-    body: JSON.stringify(arg.body) || '',
+    body: arg.body || '',
     queryString: initUrlencoded(arg.queryString) || '',
     header: arg.method || {}
   }
@@ -110,58 +110,58 @@ const apiFetch = async (arg) => {
 
 }
 
-const apiTest = async () => {
-  try {
-    const response = await fetch(`${apiHost}/`, {
-      ...d4Header,
-      method: 'GET',
-    });
-    console.log('response', response)
-    if (!response.ok) {
-      switch (response.status) {
-        case 400:
-          throw new FetchError(response, {message: 'Bad Request' });
-        case 401:
-          throw new FetchError(response, { message: 'Unauthorized' });
-        case 403:
-          throw new FetchError(response, { message: 'Forbidden' });
-        case 404:
-          throw new FetchError(response, { message: 'Not Found' });
-        case 500:
-          throw new FetchError(response, { message: 'Internal Server Error' });
-        default:
-          throw new FetchError(response, { message: 'Unknown Error' });
-      }
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    if (error instanceof FetchError) {
-      throw error;
-    } else {
-      if (error instanceof TypeError && error.message === 'Failed to fetch') {
-        // Using navigator.onLine as an additional check
-        if (!navigator.onLine) {
-          console.error('Network error: You appear to be offline.');
-          throw new FetchError('Network error', {
-            message: 'offline',
-            code: ERRORCODE.OFFLINE
-          })
-        } else {
-          console.error('Network error: Connection was reset.');
-          throw new FetchError('Network error', {
-            message: 'network error',
-            code: ERRORCODE.NETWORKERROR
-          })
-        }
-      } else {
-        console.error('Unexpected error:', error);
-      }
-      console.log(error instanceof TypeError); // true
-      console.log(error.message); // "null has no properties"
-      console.log(error.name); // "TypeError"
-      console.log(error.stack); // Stack of the error
-      console.error('Unexpected error:', error);
-    }
-  }
-}
+// const apiTest = async () => {
+//   try {
+//     const response = await fetch(`${apiHost}/`, {
+//       ...d4Header,
+//       method: 'GET',
+//     });
+//     console.log('response', response)
+//     if (!response.ok) {
+//       switch (response.status) {
+//         case 400:
+//           throw new FetchError(response, {message: 'Bad Request' });
+//         case 401:
+//           throw new FetchError(response, { message: 'Unauthorized' });
+//         case 403:
+//           throw new FetchError(response, { message: 'Forbidden' });
+//         case 404:
+//           throw new FetchError(response, { message: 'Not Found' });
+//         case 500:
+//           throw new FetchError(response, { message: 'Internal Server Error' });
+//         default:
+//           throw new FetchError(response, { message: 'Unknown Error' });
+//       }
+//     }
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     if (error instanceof FetchError) {
+//       throw error;
+//     } else {
+//       if (error instanceof TypeError && error.message === 'Failed to fetch') {
+//         // Using navigator.onLine as an additional check
+//         if (!navigator.onLine) {
+//           console.error('Network error: You appear to be offline.');
+//           throw new FetchError('Network error', {
+//             message: 'offline',
+//             code: ERRORCODE.OFFLINE
+//           })
+//         } else {
+//           console.error('Network error: Connection was reset.');
+//           throw new FetchError('Network error', {
+//             message: 'network error',
+//             code: ERRORCODE.NETWORKERROR
+//           })
+//         }
+//       } else {
+//         console.error('Unexpected error:', error);
+//       }
+//       console.log(error instanceof TypeError); // true
+//       console.log(error.message); // "null has no properties"
+//       console.log(error.name); // "TypeError"
+//       console.log(error.stack); // Stack of the error
+//       console.error('Unexpected error:', error);
+//     }
+//   }
+// }
